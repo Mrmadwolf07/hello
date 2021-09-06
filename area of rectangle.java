6291f49c724f88e332eab68f4e1ae5643e9690bf
Mrmadwolf07 <90177689+Mrmadwolf07@users.noreply.github.com>
2021-09-06 13:08:46 +0530

    Add files via upload

---
+++ area of rectangle.java
@@ -0,0 +1,27 @@
+// Program to find the area of rectangle
+public class Main
+
+{
+	
+   public static void main(String[] args) 
+
+{
+	
+   int length=70;
+	 
+   int breadth=50;
+	
+   int area=length*breadth;
+	
+		
+   System.out.println("area of rectangle" + area);
+	}
+}
+
+
+
+
+
+
+
+
